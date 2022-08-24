b40a3337b645783b7d5e82663abe7b243524c660
T45K <tasktas9@gmail.com>
2022-08-24 23:24:13 +0900

    initial commit

---
+++ Main.java
@@ -0,0 +1,38 @@
+import java.util.Optional;
+
+public class Main {
+    @SuppressWarnings("all")
+    public static void main(final String[] args) {
+        final Double answer = getGensokyoBus()
+            .map(bus -> bus.gottenOn(3)) // 紅魔館
+            .map(bus -> bus.gottenOff(1))
+            .map(bus -> bus.gottenOn(0.5)) // 白玉楼
+            .map(bus -> bus.gottenOff(2)) // 八雲さん家
+            .map(Bus::getPassengerCount)
+            .orElse(0d);
+    }
+
+    private static Optional<Bus> getGensokyoBus() {
+        return Optional.empty();
+    }
+
+    private static class Bus {
+        public double getPassengerCount() {
+            return passengerCount;
+        }
+
+        private final double passengerCount;
+
+        Bus(final double passengerCount) {
+            this.passengerCount = passengerCount;
+        }
+
+        Bus gottenOn(final double passengerCount) {
+            return new Bus(this.passengerCount + passengerCount);
+        }
+
+        Bus gottenOff(final double passengerCount) {
+            return new Bus(this.passengerCount - passengerCount);
+        }
+    }
+}
